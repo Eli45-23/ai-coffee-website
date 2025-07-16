60c6241d692d3b69bbc7917c3c0052d8425eed15
elias <eliascolon4488@gmail.com>
2025-07-16 18:36:56 -0400

    feat: add /start route placeholder

---
+++ src/pages/start.tsx
@@ -0,0 +1,8 @@
+export default function StartPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Welcome to the Onboarding Form</h1>
+      <p>This page is working! Now embed the real form here.</p>
+    </div>
+  )
+}
